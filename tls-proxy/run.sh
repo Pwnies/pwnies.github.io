3a0d54aa8ac8cc6141386a45cddc50c5c9971afa
Mathias Svensson <freaken@freaken.dk>
2017-06-01 21:13:24 +0200

    Fix run script

--- tls-proxy/run.sh
+++ tls-proxy/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -ev
 docker build -t tls-proxy .
-docker run -p 80:80 -p 443:443 -v /etc/letsencrypt:/etc/letsencrypt -it tls-proxy
+docker run -p 80:80 -p 443:443 -v /etc/letsencrypt:/etc/letsencrypt -d tls-proxy
