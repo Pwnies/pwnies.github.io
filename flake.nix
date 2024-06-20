fc33794ec2c1a96fb47bfbebda67fc1f96217f3b
Kristoffer Søholm <k.soeholm@gmail.com>
2024-06-20 20:49:55 +0200

    Remove old stuff, add flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      env = pkgs.ruby.withPackages(ps: with ps; [ jekyll jekyll-paginate kramdown kramdown-parser-gfm webrick ]);
+    in
+    {
+      devShell."${system}" = pkgs.mkShell {
+        buildInputs = [
+          env
+          ];
+      };
+    };
+}
