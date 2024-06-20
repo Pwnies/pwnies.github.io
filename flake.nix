99eb6d0c0be200a5f5b11acca8753c53a3c898b2
Kristoffer Søholm <k.soeholm@gmail.com>
2024-06-20 20:51:20 +0200

    format

--- flake.nix
+++ flake.nix
@@ -3,17 +3,22 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      env = pkgs.ruby.withPackages(ps: with ps; [ jekyll jekyll-paginate kramdown kramdown-parser-gfm webrick ]);
+      env = pkgs.ruby.withPackages (
+        ps: with ps; [
+          jekyll
+          jekyll-paginate
+          kramdown
+          kramdown-parser-gfm
+          webrick
+        ]
+      );
     in
     {
-      devShell."${system}" = pkgs.mkShell {
-        buildInputs = [
-          env
-          ];
-      };
+      devShell."${system}" = pkgs.mkShell { buildInputs = [ env ]; };
     };
 }
